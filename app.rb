a6090836e5cde1e6fb8aaf9520c2151e26daaf3d
franciscocaballero <caballerofrancisco22@yahoo.com>
2017-02-15 20:39:25 -0500

    edit update

--- app.rb
+++ app.rb
@@ -26,6 +26,17 @@ get '/pokemon/:id' do
   erb :"pokemon/show"
 end
 
+get '/pokemon/:id/edit' do
+  @pokemon = Pokemon.find(params[:id])
+  erb :"pokemon/edit"
+end
+
+put '/pokemon/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  @pokemon.update(params[:pokemon])
+  redirect "/pokemon/#{@pokemon.id}"
+end
+
 get '/' do
   redirect "/pokemon"
 end
